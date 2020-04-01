572206ae85aee0cc0d0023b5d2f27cee0b0c0ebe
Karine Valença <valenca.karine@gmail.com>
2020-04-01 16:21:22 -0300

    added new methods to get etcd services

--- main.go
+++ main.go
@@ -43,19 +43,22 @@ func main() {
 	kv2, err := cli.Get(context.TODO(), "/prod_version", clientv3.WithPrefix())
 	prod_version := path.Base(string(kv2.Kvs[0].Key))
 
-	
-	
-	
 	if err != nil {
     	fmt.Print(err)
 	}
 
 	defer cli.Close()
+
 	
-	versionsChan := make(chan []string, 0)
-	go watchUpdatedVersions("/prod_version", cli, versionsChan)
+	
 	generateAlertFile(pilot_version, prod_version)
-	fmt.Print(versionsChan)
+	versionsChan := make(chan string, 0)
+	go watchUpdatedVersions("/prod_version", cli, versionsChan)
+	//go watchVersions("/prod_version", cli, versionsChan)
+	ver := <- versionsChan
+	fmt.Print(ver)
+
+	
 }
 
 func generateAlertFile(pilot_version string, prod_version string) {
@@ -76,12 +79,40 @@ func generateAlertFile(pilot_version string, prod_version string) {
 }
 
 //it works, but isn't being called by go thread
-func watchUpdatedVersions(version string, cli *clientv3.Client, versionsChan chan []string){
+func watchUpdatedVersions(version string, cli *clientv3.Client, versionsChan chan string){
 	fmt.Print("function called")
 	rch := cli.Watch(context.Background(), version)
+	var prod_version string
 	for wresp := range rch {
-    	for _, ev := range wresp.Events {
-        	fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
-    	}
+		fmt.Print(wresp)
+		for _, ev := range wresp.Events {
+			prod_version = string(ev.Kv.Key)
+			fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+		}
+		versionsChan <- prod_version
+	}	
+}
+
+func watchVersions(version string, cli *clientv3.Client, versionsChan chan string) {
+	watchChan := cli.Watch(context.TODO(), version, clientv3.WithPrefix())
+	for {
+		fmt.Print("Versions updated")
+		rsp, err0 := cli.Get(context.TODO(), version, clientv3.WithPrefix())
+		if err0 != nil {
+			fmt.Print(err0)
+		}
+
+		if len(rsp.Kvs) == 0 {
+			fmt.Print("no prod versions founded")
+		} else {
+			var prod_version string
+			for _, kv := range rsp.Kvs {
+				fmt.Print("Versao atualizada")
+				prod_version = path.Base(string(kv.Key))
+			}
+			versionsChan <- prod_version
+		}
+		<-watchChan
+		time.Sleep(5 * time.Second)
 	}
 }
