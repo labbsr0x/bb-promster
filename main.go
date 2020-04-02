02ab7dac5e357b78d6296e0573e87b1e16aae3b4
Karine Valença <valenca.karine@gmail.com>
2020-04-02 11:03:52 -0300

    updating version from etcd working

--- main.go
+++ main.go
@@ -16,7 +16,6 @@ type Version struct{
 }
 
 func main() {
-	
 	//etcd writing
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{"http://etcd:2379"},
@@ -46,23 +45,22 @@ func main() {
 	if err != nil {
     	fmt.Print(err)
 	}
+	
 
 	defer cli.Close()
 
 	
-	
 	generateAlertFile(pilot_version, prod_version)
-	versionsChan := make(chan string, 0)
-	go watchUpdatedVersions("/prod_version", cli, versionsChan)
-	//go watchVersions("/prod_version", cli, versionsChan)
-	ver := <- versionsChan
-	fmt.Print(ver)
-
+	
+	go watchUpdatedVersions("/prod_version", cli)
+	
+	select{}
+	
 	
 }
 
 func generateAlertFile(pilot_version string, prod_version string) {
-	fmt.Print("Generate file")
+	fmt.Println("Generate file")
 	// templating 
 	versions := Version{pilot_version, prod_version}
 	tmpl, err := template.ParseFiles("/etc/prometheus/alert-rules.yml.tmpl")
@@ -78,41 +76,52 @@ func generateAlertFile(pilot_version string, prod_version string) {
 	if err != nil { panic(err) }
 }
 
-//it works, but isn't being called by go thread
-func watchUpdatedVersions(version string, cli *clientv3.Client, versionsChan chan string){
-	fmt.Print("function called")
-	rch := cli.Watch(context.Background(), version)
-	var prod_version string
-	for wresp := range rch {
-		fmt.Print(wresp)
-		for _, ev := range wresp.Events {
-			prod_version = string(ev.Kv.Key)
-			fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
-		}
-		versionsChan <- prod_version
-	}	
+func forever() {
+    for {
+        fmt.Printf("%v+\n", time.Now())
+        time.Sleep(time.Second)
+    }
 }
 
-func watchVersions(version string, cli *clientv3.Client, versionsChan chan string) {
-	watchChan := cli.Watch(context.TODO(), version, clientv3.WithPrefix())
+//it works, but isn't being called by go thread
+func watchUpdatedVersionsDeprecated(version string, cli *clientv3.Client, versionsChan chan string){
+	//for {
+		fmt.Print("function called")
+		rch := cli.Watch(context.Background(), version)
+		var prod_version string
+		for wresp := range rch {
+			fmt.Print(wresp)
+			for _, ev := range wresp.Events {
+				prod_version = string(ev.Kv.Key)
+				fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+			}
+			versionsChan <- prod_version
+			
+		}
+		//time.Sleep(time.Second)
+	//} 
+}
+
+func watchUpdatedVersions(version string, cli *clientv3.Client) {
 	for {
-		fmt.Print("Versions updated")
 		rsp, err0 := cli.Get(context.TODO(), version, clientv3.WithPrefix())
+		
 		if err0 != nil {
 			fmt.Print(err0)
 		}
-
+		
 		if len(rsp.Kvs) == 0 {
-			fmt.Print("no prod versions founded")
-		} else {
-			var prod_version string
-			for _, kv := range rsp.Kvs {
-				fmt.Print("Versao atualizada")
-				prod_version = path.Base(string(kv.Key))
-			}
-			versionsChan <- prod_version
+			fmt.Printf("no %s versions founded\n", version)
+		} else {	
+			arraySize := len(rsp.Kvs)
+			prodPath := string(rsp.Kvs[arraySize-1].Key)
+			prodVersion := path.Base(prodPath)
+			fmt.Println("Prod version", prodVersion)
+			
+			
+			//versionsChan <- prod_version
 		}
-		<-watchChan
-		time.Sleep(5 * time.Second)
+		
+		time.Sleep(60 * time.Second)
 	}
 }
