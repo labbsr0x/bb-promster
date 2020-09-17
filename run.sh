9cd2ce6fb7d107769a78c836f16027b8c8ea2d79
Karine Valença <valenca.karine@gmail.com>
2020-09-17 08:00:43 -0300

    remove special characters from sed

--- run.sh
+++ run.sh
@@ -86,7 +86,7 @@ fi
 
 if [[ "$ALERT_MANAGER_URLS" != "" ]]; then
     sed -i -e 's;$ALERT_RULES_FILE;'"- $ALERT_RULES_FILE"';g' "/prometheus.yml.tmpl"
-	sed -i "/alerting/,/    - targets: ['$ALERT_MANAGER_URLS']/d" "/prometheus.yml.tmpl"
+	sed -i "/alerting/,/targets: /d" "/prometheus.yml.tmpl"
 	cat >> "/prometheus.yml.tmpl" <<- EOM
 
 alerting:
